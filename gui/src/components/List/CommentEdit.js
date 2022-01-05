88c360ee59892c9b3456e0415b22a46c00679572
joeldelacalle <joeldelacalle@gmail.com>
2022-01-05 18:19:39 +0100

    Modificar recursos

--- gui/src/components/List/CommentEdit.js
+++ gui/src/components/List/CommentEdit.js
@@ -3,30 +3,44 @@ import Form from 'react-bootstrap/Form';
 import FloatingLabel from 'react-bootstrap/FloatingLabel';
 import Button from 'react-bootstrap/Button';
 
-const CommentEdit = () => {
-    const [content, setContent] = useState("");
+const CommentEdit = ({setComPopup}) => {
+    const [text, setText] = useState("");
     const createComment = ({ content, postId}) => {
         //Realizar con mi clase comentario de la api de flask
     }
+    const handleExpand = (e) => {
+        e.target.style.height = "inherit";
+        e.target.style.height = `${e.target.scrollHeight}px`;
+    };
+
+    const handleOnChangeText = (e) => {
+        setText(e.target.value);
+        handleExpand(e);
+    };
+    const handleOnClick = (e) => {
+        //Llamada API
+        setComPopup(false)
+      };
     const onSubmitHandler = (event) => {
         event.preventDefault();
-        console.log("===")
-
-        //Post API(Comentarios Flask) CALL
+        console.log("===");
     };
     return (
-        <Form onSubmit={onSubmitHandler}>
-            <FloatingLabel label="Comments">
-                <Form.Control
-                    typeof="text" 
-                    id="content"
-                    value={content}
-                    onChange={e => setContent(e.target.value)}
-                    aria-describedby="content-helper-text"
-                ></Form.Control>
-                <Button type="submit" variant="primary">Comentar</Button>
-            </FloatingLabel>
-        </Form>
+    <div>
+      <textarea
+        type="text"
+        name="text"
+        id="text"
+        value={text}
+        autoComplete="off"
+        onChange={handleOnChangeText}
+        placeholder="Add Text..."
+      />
+      
+     <button onClick={handleOnClick}>
+        Send
+     </button>
+    </div>
         
          
     );
